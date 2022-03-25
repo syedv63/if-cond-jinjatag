64e18ce4c4dc4b16636c10ff347d851970567226
syed <syedv63@gmail.com>
2022-03-25 19:34:11 +0530

    first commit

--- app1/views.py
+++ app1/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def conditions(request):
-    d1={'a':10,'b':20}
+    d1={'a':10,'b':20,'c':30}
     return render(request,'conditions.html',d1)
